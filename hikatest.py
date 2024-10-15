131262564eefc56e1c728cc1c57798882a05def8
WinShop31 <167513406+WinShop31@users.noreply.github.com>
2024-10-15 21:22:49 +0300

    hikatest.py

--- hikatest.py
+++ hikatest.py
@@ -4,9 +4,11 @@ from hikkatl.types import Message
 from .. import loader, utils
 
 @loader.tds
-class MyModule(loader.Module):
-    """My module"""
-    strings = {"name": "MyModule", "key_response": 'Ваш ключ - "{key}"'}
+class BypassModule(loader.Module):
+    """Bypass module
+    🌟 Создано - @aurafunny
+    """
+    strings = {"name": "BypassModule", "key_response": 'Ваш ключ - "{key}"'}
 
     @loader.command(
         ru_doc="Использование .key <url>",
@@ -32,3 +34,22 @@ class MyModule(loader.Module):
                         await utils.answer(message, "Ошибка при обработке ответа.")
                 else:
                     await utils.answer(message, "Ошибка при запросе к сайту.")
+
+    @loader.command(
+        ru_doc="Обновить модуль",
+    )
+    async def update(self, message: Message):
+        """Обновить модуль"""
+        await utils.answer(message, "Начинаю обновление модуля...")
+        url = "https://raw.githubusercontent.com/WinShop31/nextjs-boilerplate/refs/heads/main/hikatest.py"
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                if response.status == 200:
+                    new_code = await response.text()
+                    # Здесь необходимо сохранить новый код в файл
+                    with open("hikatest.py", "w") as file:
+                        file.write(new_code)
+                    await utils.answer(message, "Модуль успешно обновлён!")
+                else:
+                    await utils.answer(message, "Ошибка при загрузке нового кода.")
